3210d8a06a42ea5f0e419c019feba253a9ff6130
DorianICG <doriancanales0602@gmail.com>
2023-05-24 01:41:22 -0400

    Lectura de las comunas desde un csv

--- src/main/java/org/example/Comuna.java
+++ src/main/java/org/example/Comuna.java
@@ -1,15 +1,18 @@
 package org.example;
 
+import com.opencsv.exceptions.CsvValidationException;
+
+import java.io.*;
 import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.Scanner;
 
 public class Comuna {
     private int id;
     private String nombreComuna;
-    private int costo;
-    private ArrayList<Comuna>comunasColindantes = new ArrayList<>();
+    private float costo;
+    private ArrayList<Integer>comunasColindantes = new ArrayList<>();
 
-    public Comuna(int id, String nombreComuna, int costo, ArrayList<Comuna> comunasColindantes) {
+    public Comuna(int id, String nombreComuna, float costo, ArrayList<Integer> comunasColindantes) {
         this.id = id;
         this.nombreComuna = nombreComuna;
         this.costo = costo;
@@ -35,20 +38,45 @@ public class Comuna {
         this.nombreComuna = nombreComuna;
     }
 
-    public int getCosto() {
+    public float getCosto() {
         return costo;
     }
 
-    public void setCosto(int costo) {
+    public void setCosto(float costo) {
         this.costo = costo;
     }
 
-    public ArrayList<Comuna> getComunasColindantes() {
+    public ArrayList<Integer> getComunasColindantes() {
         return comunasColindantes;
     }
 
-    public void setComunasColindantes(ArrayList<Comuna> comunasColindantes) {
+    public void setComunasColindantes(ArrayList<Integer> comunasColindantes) {
         this.comunasColindantes = comunasColindantes;
     }
 
+    public void leerComunas(ArrayList<Comuna>comunas) throws CsvValidationException, FileNotFoundException {
+        try {
+            Scanner scan = new Scanner(new File("src/test/java/Comunas.csv"));
+            scan.useDelimiter(";;;"); // usar ";;;" como delimitador de línea
+            scan.nextLine(); // saltar el encabezado
+            while (scan.hasNextLine()) {
+                String linea = scan.nextLine();
+                linea = linea.replaceAll(";;;", ""); // eliminar el delimitador del final
+                String[] datos = linea.split(","); // separar los datos por comas
+
+                    int idNueva = Integer.parseInt(datos[0]); // convertir el primer dato a int
+                    String nombreNuevo = datos[1]; // el segundo dato ya es String
+                    float costoNuevo = Float.parseFloat(datos[2]); // convertir el tercer dato a float
+                    String[] arregloStr = datos[3].split("/"); // separar el arreglo de enteros por barras
+                    ArrayList<Integer> arregloNuevo = new ArrayList<Integer>(); // crear un ArrayList de Integer
+                    for (String s : arregloStr) {
+                        arregloNuevo.add(Integer.parseInt(s)); // convertir cada elemento del arreglo a int y agregarlo al ArrayList
+                    }
+                    comunas.add(new Comuna(idNueva,nombreNuevo,costoNuevo,arregloNuevo));
+            }
+        }catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,21 @@
 package org.example;
 
+import java.util.ArrayList;
+import com.opencsv.exceptions.CsvValidationException;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws CsvValidationException, FileNotFoundException  {
+        ArrayList<Comuna>comunas=new ArrayList<>();
+        Comuna comuna = new Comuna();
+        Scanner scanner=new Scanner(System.in);
+        comuna.leerComunas(comunas);
+        for(Comuna comunita: comunas){
+            System.out.println("ID: "+comunita.getId()+"\nNombre: "+comunita.getNombreComuna()+"\nCosto: "+comunita.getCosto());
+            for(Integer comunitasColindantes: comunita.getComunasColindantes())
+                System.out.print(comunitasColindantes + " - ");
+            System.out.println();
+        }
     }
 }
